aea0152dfb2c816b49383d8496bf92e3fb78ffba
Timothy Duong <tim.duong@mail.utoronto.ca>
2019-02-23 23:55:55 -0500

    welp

--- DataProcessor/table_creation.sql
+++ DataProcessor/table_creation.sql
@@ -2,10 +2,21 @@ CREATE DATABASE IF NOT EXISTS product_recs;
 
 USE product_recs;
 
+DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Recommendation;
 DROP TABLE IF EXISTS Rating;
 DROP TABLE IF EXISTS Product;
 DROP TABLE IF EXISTS Receipt;
+
+CREATE TABLE IF NOT EXISTS User (
+  id varchar(255),
+  firstName varchar(255),
+  lastName varchar(255),
+  email varchar(255),
+  income float,
+  PRIMARY KEY(id)
+);
+
 CREATE TABLE IF NOT EXISTS Product
 (
   id varchar(255),
@@ -25,7 +36,9 @@ CREATE TABLE  IF NOT EXISTS Rating
   rating int,
   PRIMARY KEY(productId, userId),
   FOREIGN KEY (productId) 
-    REFERENCES Product(id)
+    REFERENCES Product(id),
+  FOREIGN KEY (userId)
+    REFERENCES User(id)
 );
 
 CREATE TABLE  IF NOT EXISTS Recommendation
@@ -35,7 +48,9 @@ CREATE TABLE  IF NOT EXISTS Recommendation
   prediction float,
   PRIMARY KEY(userId, productId),
   FOREIGN KEY (productId) 
-    REFERENCES Product(id)
+    REFERENCES Product(id),
+  FOREIGN KEY (userId)
+    REFERENCES User(id)
 );
 
 CREATE TABLE IF NOT EXISTS Receipt
@@ -50,5 +65,7 @@ CREATE TABLE IF NOT EXISTS Receipt
   tax float,
   PRIMARY KEY (id),
   FOREIGN KEY (productId) 
-    REFERENCES Product(id)
+    REFERENCES Product(id),
+  FOREIGN KEY (userId)
+    REFERENCES User(id)
 );
